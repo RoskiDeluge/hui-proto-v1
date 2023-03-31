2e2f7ef1603e3c5418396b8b772b2812de40efb8
RoskiDeluge <roberto.delgado@gmail.com>
2023-03-30 14:44:13 -1000

    First commit, finished main nav

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    images: {
+        domains: ["aleph-m-2.s3.amazonaws.com", "fakestoreapi.com"]
+    }
+}
---
+++ src/components/Header.js
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import {
+    MenuIcon,
+    SearchIcon,
+    ShoppingCartIcon
+} from "@heroicons/react/outline"
+
+function Header() {
+  return (
+    <header>
+        {/* Top nav */}
+        <div className="flex items-center p-1 py-2 flex-grow bg-amazon_blue">
+            <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+                <Image 
+                    src="https://aleph-m-2.s3.amazonaws.com/hui_logo.png" 
+                    width={70} 
+                    height={25} 
+                    objectFit="contain" 
+                    alt="logo"
+                    className="cursor-pointer"
+                />
+            </div>
+            {/* Search  */}
+            <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-[#00A8BD]">
+                <input className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4" type="text" />
+                <SearchIcon className="h-12 p-4" />
+            </div>
+
+            {/* Right  */}
+            <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+                <div className="link">
+                    <p>Hello, Roberto Delgado</p>
+                    <p className="md:text-sm">Account & Lists</p>
+                </div>
+                <div className="link">
+                    <p className="md:text-sm">Rentals</p>
+                </div>
+                <div className="relative link flex items-center">
+                    <span className="absolute top-0 right-0 md:right-6 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">0</span>
+                    <ShoppingCartIcon className="h-10" />
+                    <p className="hidden md:inline mt-2">Cart</p>
+                </div>
+            </div>
+        </div>
+        {/* Bottom nav */}
+        <div></div>
+    </header>
+  )
+}
+
+export default Header
